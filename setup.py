c6459031b43e95522a3257efe3d492226531df6b
Alexander Plavin <alexander@plav.in>
2015-12-11 17:30:40 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='wncc',
+    version='1.0',
+    packages=['wncc'],
+    url='https://github.com/aplavin/wncc',
+    license='MIT',
+    author='Alexander Plavin',
+    author_email='alexander@plav.in',
+    description='Weighted normalized cross-correlation'
+)
